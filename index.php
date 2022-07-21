4ba97de63bd2ffcfedb8f1d294a8ff5595656f3c
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-07-21 21:05:54 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>Pratham Gupta MD5</title>
+</head>
+<body>
+    <h1>MD5 cracker</h1>
+    <p>The web application takes an MD5 hash of a four digit pin 
+        and checks all 10,000 possible 4 digit PIN's to determine the pin!
+    </p>
+    <pre>
+        Debug Output:
+        <?php
+
+        ?>
+    </pre>
+</body>
